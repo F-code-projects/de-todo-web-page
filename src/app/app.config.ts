a45e604e00cfcfe2c5cb879508d97000351b712d
FrancoCarrillo <u202013943@upc.edu.pe>
2025-01-19 20:55:41 -0500

    feat: add server side rendering configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,14 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import Aura from '@primeng/themes/aura';
 import { provideHttpClient } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideRouter(routes),
     provideClientHydration(),
